ddc6e681d5a2520647389e2372b9343cd1aa7506
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-05-06 01:18:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,361 @@
+let data={
+    "sections":[
+        {
+            "section title":"General Knowledge",
+            "questions":[{
+            "questionType":"mcq",
+            "question":"Numismatics is the study of :",
+            "options":["Coins","Numbers","Stamps","SpaceAns"],
+            "answer":"Coins"},
+            {
+                "questionType":"text",
+                "question":"Sound travels fastest through which particle state ?",
+                "answer":"solid"},
+        {
+            "questionType":"number",
+            "question":"On which date of december noble prize is awarded?",
+            "answer":10},        
+        {
+            "questionType":"mcq",
+            "question":"Who is the designer of India’s new currency symbol?",
+            "options":[ "D Ramesh Kumar","D Udaya Kumar","D Abhay Kumar","M Diwaka"],
+            "answer":"D Udaya Kumar"},
+    {
+            "questionType":"text",
+            "question":"The Harry Potter series is written by?",
+            "answer":"J K Rowling"},
+ {
+            "questionType":"text",
+            "question":"The language with the richest vocabulary?",
+            "answer":"English"},
+        {
+            "questionType":"number",
+            "question":"India plans a manned mission to the moon by?",
+            "answer":2040},
+       {
+            "questionType":"number",
+            "question":"How many medals were won by India in Olympics history?",
+            "answer":20},
+        {
+            "questionType":"mcq",
+            "question":"The Manas Wild Life sanctuary is located in the state of ?",
+            "options":["Gujarat","Madhya Pradesh","Uttar Pradesh","Assam"],
+            "answer":"Assam"},    
+      {
+           "questionType":"mcq",
+           "question":"Arena is the special name for playground for?",
+           "options":["Cricket","Lawn Tennis","Wrestling","Skating"],
+          "answer":"Wrestling"}         
+       
+        ]
+        },
+        /// next section science
+        {
+            "section title":"Science",
+            "questions":[{
+            "questionType":"mcq",
+            "question":"Bones help to protect our vital organs?",
+            "options":["Heart","Liver","Lungs","All of these"],
+            "answer":"All of these"},
+            {
+                "questionType":"text",
+                "question":"The food we chew passes down into a sac like structure called?",
+                "answer":"Stomach"},
+        {
+            "questionType":"number",
+            "question":`Which body part pumps blood to all parts of the body?
+            1.Stomach 2.Brain 3.Heart 4.Lungs (input option no.)`,
+            "answer":3},        
+        {
+            "questionType":"mcq",
+            "question":"Lungs are important because they ____?",
+            "options":[ "Contract & Expand","Absorb oxygen","Release carbon dioxide","Both A & B"],
+            "answer":"Absorb oxygen"},
+    {
+            "questionType":"text",
+            "question":"Which organ is control centre of the body?",
+            "answer":"Brain"},
+            {
+            "questionType":"text",
+            "question":"Plants absorb?",
+            "answer":"Carbon dioxide"},
+        {
+            "questionType":"number",
+            "question":`Human being & animals move from one place to another, this is called?
+            1.Movement 2.Reproduction 3.Locomotion 4.Variation (enter Option no.)`,
+            "answer":3},
+       {
+            "questionType":"number",
+            "question":`Which one provides instant energy to our body to perform work?
+            1.Proteins 2.Carbohydrates 3.Fats 4.Vitamins (enter option no.)`,
+            "answer":3},
+        {
+            "questionType":"mcq",
+            "question":"Which one of the following helps in growth and healing of wounds?",
+            "options":["Proteins","Carbohydrates","Fats","Vitamins"],
+            "answer":"Proteins"},    
+      {
+           "questionType":"mcq",
+           "question":"A diet which contains all the foods groups in proper amount is called?",
+           "options":["Unbalanced diet","Balanced diet","Food Pyramid","Hygiene"],
+          "answer":"Balanced diet"}         
+       
+        ]
+        },  
+     /// next section maths
+     {
+        "section title":"Mathematics",
+        "questions":[{
+        "questionType":"mcq",
+        "question":"(n + 1)^2 – 1 is divisible by 8, if n is?",
+        "options":["an odd integer","an even integer","a natural number","an integer"],
+        "answer":"an odd integer"},
+        {
+            "questionType":"text",
+            "question":`The graph of y = f(x), where f(x) is a quadratic polynomial meets the  x-axis at A(–2, 0) and B(–3,0), then the expression for f(x) is?? 
+                    (a) x^2+5x+6 (b) x^2-5x+6       (c)x^2+5x-6       (d) x^2-5x–6 (enter option)`,
+            "answer":"b"},
+    {
+        "questionType":"number",
+        "question":`The pair of equations x = a and y = b graphically representing lines which are :
+                  (1) coincident   (2) intersecting at (a, b)          (3) parallel    (4) intersecting at (b, a) (input option no.)`,
+        "answer":2},        
+    {
+        "questionType":"mcq",
+        "question":"The sum and product of the roots of 2x2– 5x + 4 = 0are in the ratio ?",
+        "options":[ "5:2","5:4","10:4","none of these"],
+        "answer":"5:4"},
+{
+        "questionType":"text",
+        "question":`If 11 times the 11th term of an A.P. is equal to 7times its 7th term, then its 18th term will be?`,
+        "answer":0},
+        {
+        "questionType":"text",
+        "question":`If the points A(1, 2), B(0, 0) and C(a, b) are collinear, then :   
+         (a)a = b   (b)a = 2b  (c)2a = b (d)a = –b (input option)`,
+        "answer":"c"},
+    {
+        "questionType":"number",
+        "question":`For a given data with 50 observations the ‘less than ogive’ and the ‘more then ogive’ intersect at (15.5, 20). The median of the data is?`,
+        "answer":15.5},
+   {
+        "questionType":"number",
+        "question":`sin (45° + θ) – cos (45° – θ) is equal to`,
+        "answer":0},
+    {
+        "questionType":"mcq",
+        "question":"In a throw of two dice, the probability of getting a sum of 10 is?",
+        "options":["1/12","1/36","1/6", " 1/4 "],
+        "answer":"1/12"},    
+  {
+       "questionType":"mcq",
+       "question":"The radii of bases of a cylinder and a cone are in the ratio 3 : 4 and  their heights are in the ratio     2 : 3. The ratio between the volume of  cylinder to that of cone is?",
+       "options":["8:9","9:8","5:7 ","7:5"],
+      "answer":"9:8"}         
+   
+    ]
+    },  
+    /// next section history
+    {
+        "section title":"Indian History",
+        "questions":[{
+        "questionType":"mcq",
+        "question":"Where is the Indus Civilization city Lothal ?",
+        "options":["Gujarat","Rajasthan","Punjab","Haryana"],
+        "answer":"Gujarat"},
+        {
+            "questionType":"text",
+            "question":`Mohenjo Daro is situated in
+            (A) Sindh Province of Pakistan(B) Gujarat(C) Punjab(D) Afghanistan
+            (enter option)`,
+            "answer":"A"},
+    {
+        "questionType":"number",
+        "question":`Which deity was not worshipped by the Vedic Aryans ?
+        (1) Indra(2) Marut(3) Varun(4) Pashupati
+        (input option no.)`,
+        "answer":4},        
+    {
+        "questionType":"mcq",
+        "question":"The earliest available work of the Sangam Tamils is?",
+        "options":[ "Pattinappalai","Tirumurugarruppadai","Maduraikanchi","Tolkappiyam"],
+        "answer":"Tolkappiyam"},
+{
+        "questionType":"text",
+        "question":`The Mahavir belonged to the clan?
+        (A) Kalams(B) Bhaggas(C) Lichhivis (D) Bulis (input option letter)`,
+        "answer":"C"},
+        {
+        "questionType":"text",
+        "question":`. The Jain text which contains the biographies of the Tirthankaras is known as
+        (A) Bhagwatisutra(B) Uvasagadasao(C) Adi Purana(D) Kalpasutra (input option)`,
+        "answer":"D"},
+    {
+        "questionType":"number",
+        "question":`The first Buddhist Sangeeti (conference) was held at
+        (1) Vaishali(2) Pataliputra(3) Rajgriha (4) Ujjain (input option no.)`,
+        "answer":3},
+   {
+        "questionType":"number",
+        "question":`The propounder of the Madhyamika Philosophy was
+        (1) Bhadrabahu (2) Parshwanath (3) Sheelbhadra (4) Nagarjuna (input option no.)`,
+        "answer":4},
+    {
+        "questionType":"mcq",
+        "question":"The rules of Buddhist monistic life are laid down, primarily, in",
+        "options":["Tripitaka","Vinayapitaka","Abhidhammapitaka","Suttapitaka"],
+        "answer":"Vinayapitaka"},    
+  {
+       "questionType":"mcq",
+       "question":"The battle between Alexander and Porus took place on the bank of river",
+       "options":["Sutlej","Ravi","Jhelum","Ganga"],
+      "answer":"Jhelum"}         
+    ]
+    }              
+    ]
+}
+document.addEventListener("DOMContentLoaded",function(){
+initsection();
+let maxScore=0;
+function initsection(){
+    let allsect=document.querySelectorAll(".section");
+    allsect.forEach((section) => {
+        section.addEventListener("click",()=>{
+            let sectionNum=parseInt(section.getAttribute("data-section"));
+            startquiz(sectionNum);
+        });
+    });
+}
+function startquiz(index){
+    let currQues=data.sections[index].questions;
+    console.log(currQues);
+    let currQuesIndex=0;
+    let score=0;
+    let ansSelected=false;
+    let checker=true;
+    let ansSelectedArray=[];
+    for (let i = 0; i < currQues.length; i++) {
+        ansSelectedArray.push(0);
+    }
+    document.getElementById("quiz-container").style.display="none";
+    document.getElementById("question-container").style.display="block";
+    document.getElementById('question-container').innerHTML=` <p id="score">Score:0</p>
+    <div id="question"></div>
+    <div id="options"></div>
+    <button id="next-btn">Next</button>
+    <button id="pre-btn">Back</button>`;
+    showQuestion();
+    function showQuestion(){
+        const question=currQues[currQuesIndex];
+        let quesElement=document.getElementById("question");
+        let optionsElement=document.getElementById("options");
+
+      quesElement.textContent=question.question;
+      optionsElement.innerHTML='';
+      if (question.questionType==='mcq') {
+        question.options.forEach((option,i)=>{
+    const optionElement=document.createElement("div");
+    optionElement.textContent=option;
+    optionElement.addEventListener("click",()=>{
+if (!ansSelected && ansSelectedArray[currQuesIndex]==0) {
+    ansSelected=true;
+    checker=true;
+    ansSelectedArray[currQuesIndex]=1;
+optionElement.classList.add('selected');
+checkanswer(option,question.answer);
+}
+else if(checker){
+    checker=false;
+    const feedbackElem=document.createElement('div');
+        feedbackElem.id='feedback';
+        feedbackElem.textContent='Already Submitted!!!';
+        feedbackElem.style.color='#f1c40f';
+        optionsElement.appendChild(feedbackElem);
+}
+    });
+    optionsElement.appendChild(optionElement);
+        }); 
+      }
+      else{// for text,no. input
+const inputElem=document.createElement('input');
+inputElem.type=question.questionType==='number'?'number':'text';
+const submitbtn=document.createElement('button');
+submitbtn.textContent='Submit Answer';
+submitbtn.className='submit-answer';
+submitbtn.onclick=()=>{
+    if (!ansSelected && ansSelectedArray[currQuesIndex]==0) {
+        ansSelected=true;
+        checker=true;
+        ansSelectedArray[currQuesIndex]=1;
+        checkanswer(inputElem.value.toString(),question.answer.toString());
+    }
+    else if(checker){
+        checker=false;
+        const feedbackElem=document.createElement('div');
+            feedbackElem.id='feedback';
+            feedbackElem.textContent='Already Submitted!!!';
+            feedbackElem.style.color='#f1c40f';
+            optionsElement.appendChild(feedbackElem);
+    }
+};
+optionsElement.appendChild(inputElem);
+optionsElement.appendChild(submitbtn);
+      }
+     
+    }
+    function checkanswer(a,b){
+        const feedbackElem=document.createElement('div');
+        feedbackElem.id='feedback';
+        if(a==b || a.toLowerCase()==b.toLowerCase()){
+           score++;
+           feedbackElem.textContent='Correct!';
+           feedbackElem.style.color='green';
+        }
+        else{
+            feedbackElem.textContent=`Wrong. Correct Answer: ${b}`;
+            feedbackElem.style.color='red';
+        }
+        const optionsElement=document.getElementById('options');
+        optionsElement.appendChild(feedbackElem);
+        updatescore();
+    }
+    function updatescore(){
+document.getElementById('score').textContent=`score: ${score}`;
+    }
+    document.getElementById("next-btn").addEventListener("click",()=>{
+        currQuesIndex++;
+if (currQuesIndex>=currQues.length) {
+  console.log("over");  
+  endquiz();
+}
+else{
+    ansSelected=false;
+    checker=true;
+    showQuestion();
+}
+    });
+    document.getElementById("pre-btn").addEventListener('click',()=>{
+        if(currQuesIndex>0 && currQuesIndex<currQues.length){
+            checker=true;
+currQuesIndex--;
+showQuestion();}
+    });
+    function endquiz(){
+        if (score>maxScore) {
+            maxScore=score;
+        }
+        
+        document.getElementById('question-container').innerHTML=`
+        <h1>Quiz Completed!</h1>
+        <p> your final score: ${score}/ ${currQues.length}</p>
+        <p id='mx'>Max Score: ${maxScore}</p>
+        <button id=home-btn>Go to Home</button>`;
+        document.getElementById('mx').style.cssText="font-weight:800; font-size:30px; color: #f58704;";
+        document.getElementById('home-btn').addEventListener('click',()=>{
+        document.getElementById('quiz-container').style.display='grid';
+        document.getElementById('question-container').style.display='none';
+        });
+        }
+}
+
+});
